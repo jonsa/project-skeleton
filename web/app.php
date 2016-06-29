495cc4a538777eb49f04de36bbc33a8f37ef1ce3
Jonas Sandström <sandstrm.j@gmail.com>
2016-06-29 22:54:37 +0200

    Created project skeleton

---
+++ web/app.php
@@ -0,0 +1,7 @@
+<?php
+
+if (php_sapi_name() === 'cli-server' && is_file(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH))) {
+    return false;
+}
+
+$app = require __DIR__ . '/../config/bootstrap.php';
